425d0f476212294ba6545501416760dcced5746a
daljeet-coder <daljeetsingh.chamiyal@gmail.com>
2023-02-24 11:23:20 +0530

    updates

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/user",userRoute)
 
 
 
-let PORT = process.env.PORT || 5000
+let PORT = process.env.PORT || 8080
 app.listen(PORT,async()=>{
     try{
         await connect(process.env.MONGO_URL)
